2f2baface90a19cecd10ede2ffd2eecfe306e6b6
Inferno2094 <xtebanleon2094@gmail.com>
2019-03-21 20:27:30 -0500

    Iniciando Proyecto

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>holi {{ msg }}</h1>
+        <input type="text" v-model="msg">
+        <button @click="guerdar">Guardar</button>
+        <table>
+            <tr>
+                <th>Nombre</th>
+            </tr>
+            <tr v-for="(item, index) in municipios" :key="index">
+                <td>{{item}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg: "Holi",
+            municipios: ['Mocoa', 'Villa', 'Puerto']
+        };
+    },
+    methods: {
+        guerdar(){
+            this.municipios.push(this.msg)
+            this.msg = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
